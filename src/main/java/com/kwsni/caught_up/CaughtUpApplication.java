4687bf711cb087808bd78c37759041ea39f6ef1a
kwsni <44220303+kwsni@users.noreply.github.com>
2025-08-14 12:26:07 -0700

    Initial commit

---
+++ src/main/java/com/kwsni/caught_up/CaughtUpApplication.java
@@ -0,0 +1,13 @@
+package com.kwsni.caught_up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaughtUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaughtUpApplication.class, args);
+	}
+
+}
